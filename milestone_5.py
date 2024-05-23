b068933702577c2acd171a7c46d39d57c5a29dc9
JordanNikomedes <141562779+JordanNikomedes@users.noreply.github.com>
2024-05-23 13:26:27 +0000

    updated after the code review

--- milestone_5.py
+++ milestone_5.py
@@ -1,15 +1,20 @@
 import random
 
+""" This is my hangman class consisting of all the rules and functions
+for the game """
+
 class Hangman:
 
+""" I have initiated the attributes to use throughout the class """
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
-        self.word = list(random.choice(self.word_list).lower())
+        self.word = random.choice(self.word_list).lower()
         self.word_guessed = ['_']*len(self.word)
         self.list_of_guesses = []
         self.num_letters = len(set(self.word))
-
+""" In this method i checked if the letter is in the word using a for loop to break
+down the word and an if statement to judge what action to take """
     def check_guess(self, guess):
         guess = guess.lower()
         
@@ -27,12 +32,8 @@ class Hangman:
                     print(f'You have {self.num_lives} lives left')
                     break
                     
-                
-
             self.num_letters -= 1
                 
-
-
 
     def ask_for_input(self):
         while True:
@@ -41,27 +42,35 @@ class Hangman:
             if self.guess.isalpha() == False:
                 print('Invalid letter. Please enter a single alphabetical letter')
                 continue
+
             elif self.guess in self.list_of_guesses:
                 print('You already tried that letter!')
                 continue
+
             else:
                 self.check_guess(self.guess)
                 self.list_of_guesses.append(self.guess)
+                break
 
-
+""" This function is calling the hangman class with an if statement to end the game """
 def play_game(word_list):
     game = Hangman(word_list, num_lives=5)
 
     while True:
         if game.num_lives == 0:
             print('You Lost!!!')
+            break
+
         elif game.num_letters > 0:
             game.ask_for_input()
             continue
+
         elif game.num_lives > 0 and game.num_letters == 0:
             print('Congratulations, You won the game!!!')
+            break
 
-
+""" the if name = main method is made sure i can run this program straight
+from this python file """
 if __name__ == '__main__':
     word_list = ['apple', 'grapes', 'hello', 'people', 'cars']
     play_game(word_list)
